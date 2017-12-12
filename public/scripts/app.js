7ff72a416001f4f294be3104a3552f1e8fd8d1ab
Ryan Achten <ryanachten@gmail.com>
2017-12-13 10:43:56 +1300

    Init commit of react test

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('App.js working');
+
+var template = React.createElement(
+  'h1',
+  null,
+  'Indecision App'
+);
+var appRoot = document.getElementById('app');
+
+ReactDOM.render(template, appRoot);
