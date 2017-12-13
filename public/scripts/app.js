42291fd0d871b46f85b70a369ffd96e6334379fa
Ryan Achten <ryanachten@gmail.com>
2017-12-13 19:04:39 +1300

    Add conditional expressions to app

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,11 +2,80 @@
 
 console.log('App.js working');
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'A React test app',
+  options: ['One', 'Two']
+};
+
 var template = React.createElement(
-  'h1',
+  'div',
   null,
-  'Indecision App'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  app.subtitle && React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.options.length > 0 ? 'Here are your options:' : 'No options'
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var user = {
+  name: 'Ryan',
+  age: 25,
+  location: 'Wellington'
+};
+
+var getLocation = function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+};
+
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  user.age && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,6 +1,43 @@
 console.log('App.js working');
 
-var template = <h1>Indecision App</h1>;
-var appRoot = document.getElementById('app');
+const app = {
+  title: 'Indecision App',
+  subtitle: 'A React test app',
+  options: ['One', 'Two']
+};
+
+const template = (
+  <div>
+    <h1>{ app.title }</h1>
+    { app.subtitle && <p>{ app.subtitle }</p>}
+    <p>{ app.options.length > 0 ? 'Here are your options:' : 'No options' }</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+);
+
+const user = {
+  name: 'Ryan',
+  age: 25,
+  location: 'Wellington'
+};
+
+const getLocation = ( location ) => {
+  if (location) {
+    return <p>Location: {location}</p>;
+  }
+}
+
+const templateTwo = (
+  <div>
+    <h1>{ user.name ? user.name : 'Anonymous' }</h1>
+    { (user.age && user.age >= 18) && <p>Age: { user.age }</p> }
+    { getLocation( user.location )}
+  </div>
+);
+
+const appRoot = document.getElementById('app');
 
 ReactDOM.render( template, appRoot );
