9e511e0e4e8b6d49d4d684f5b84bdb7193b97fe7
Ryan Achten <ryanachten@gmail.com>
2017-12-14 15:14:29 +1300

    Add random decision mechanism

--- src/app.js
+++ src/app.js
@@ -23,6 +23,11 @@ const onRemoveAll = () => {
   render();
 };
 
+const onMakeDecision = () => {
+    var randIndex = Math.floor(Math.random() * app.options.length);
+    alert(app.options[randIndex]);
+};
+
 const appRoot = document.getElementById('app');
 
 const render = () => {
@@ -31,11 +36,11 @@ const render = () => {
       <h1>{ app.title }</h1>
       { app.subtitle && <p>{ app.subtitle }</p>}
       <p>{ app.options.length > 0 ? 'Here are your options:' : 'No options' }</p>
-      <p>{ app.options.length }</p>
+      <button disabled={ app.options.length === 0 } onClick={ onMakeDecision }>What should I do?</button>
       <button onClick={ onRemoveAll }>Remove All</button>
+
       <ol>
-        <li>Item one</li>
-        <li>Item two</li>
+        { app.options.map( (option) => <li key={ option }>{ option }</li> ) }
       </ol>
 
       <form onSubmit={ onFormSubmit }>
