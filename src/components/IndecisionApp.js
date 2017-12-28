866a6bdb0659a20392f09538c9827693279f8e02
Ryan Achten <ryanachten@gmail.com>
2017-12-29 08:50:02 +1300

    Add transform-class-properties babel plugin for ES6 class props

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -6,18 +6,38 @@ import Header from './Header';
 import Action from './Action';
 
 export default class IndecisionApp extends React.Component {
-  constructor(props){
-    super(props);
-    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
-    this.handleDeleteOption = this.handleDeleteOption.bind(this);
-    this.handlePick = this.handlePick.bind(this);
-    this.handleAddOption = this.handleAddOption.bind(this);
-    this.state = {
-      options : []
-    };
-  }
+  state = {
+    options : []
+  };
+
+  handleDeleteOptions = () => {
+    this.setState(()=>( { options : [] } ));
+  };
+  handleDeleteOption = (option) => {
+    console.log('hdo', option);
+    this.setState(( prevState )=>{
+      const newOptions = prevState.options.filter((curOption)=>
+        curOption !== option );
+      return { 'options' : newOptions }
+    });
+  };
+  handlePick = () => {
+    const randIndex = Math.floor( Math.random() * this.state.options.length);
+    const option = this.state.options[randIndex];
+    alert(option);
+  };
+  handleAddOption = (option) => {
+    if (!option) {
+      return 'Enter valid value';
+    }else if( this.state.options.indexOf(option) > -1 ){
+      return 'This option already exists';
+    }
+    this.setState( (prevState)=>({
+        options : prevState.options.concat([option])
+      }));
+  };
+
   componentDidMount(){
-    console.log('Fetching data');
     const json = localStorage.getItem('options');
     const options = JSON.parse(json);
     try {
@@ -25,7 +45,6 @@ export default class IndecisionApp extends React.Component {
         this.setState(() => ({ options }) );
       }
     } catch (e) {
-      // console.log(e);
     }
   }
   componentDidUpdate(prevProps, prevState){
@@ -35,35 +54,6 @@ export default class IndecisionApp extends React.Component {
       localStorage.setItem('options', json);
     }
   }
-
-
-  handleDeleteOptions(){
-    this.setState(()=>( { options : [] } ));
-  }
-  handleDeleteOption(option){
-    console.log('hdo', option);
-    this.setState(( prevState )=>{
-      const newOptions = prevState.options.filter((curOption)=>
-        curOption !== option );
-      return { 'options' : newOptions }
-    });
-  }
-  handlePick(){
-    const randIndex = Math.floor( Math.random() * this.state.options.length);
-    const option = this.state.options[randIndex];
-    alert(option);
-  }
-  handleAddOption(option){
-    if (!option) {
-      return 'Enter valid value';
-    }else if( this.state.options.indexOf(option) > -1 ){
-      return 'This option already exists';
-    }
-    this.setState( (prevState)=>({
-        options : prevState.options.concat([option])
-      }));
-  }
-
   render(){
     const subtitle = "This is the subtitle content";
 
