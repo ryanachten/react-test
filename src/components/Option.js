2295012799ecf492ec39bdd2213be91aa9740d00
Ryan Achten <ryanachten@gmail.com>
2018-01-03 11:35:34 +1300

    Add option styles

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,8 +1,8 @@
 import React from 'react';
 
 const Option = (props) => (
-  <div>
-    { props.optionVal }
+  <div className="option">
+    <p className="option__text">{ props.count }. {props.optionVal }</p>
     <button
       className="button button--link"
       onClick={ (e) => {
--- src/components/Options.js
+++ src/components/Options.js
@@ -4,14 +4,19 @@ import Option from './Option';
 const Options = (props) => {
   return(
     <div>
-      <button
-        className="button button--link"
-        onClick={ props.handleDeleteOptions }>Remove All</button>
-      {props.options.length === 0 && <p>Add an option to get started</p>}
-      { props.options.map( (option) => (
+      <div className="widget-header">
+        <h3 className="widget-header__title">Your Options</h3>
+        <button
+          className="button button--link"
+          onClick={ props.handleDeleteOptions }>Remove All</button>
+      </div>
+      {props.options.length === 0 &&
+        <p className="widget__message">Add an option to get started</p>}
+      { props.options.map( (option, index) => (
         <Option
           key={option}
           optionVal={ option }
+          count={ index+1 }
           handleDeleteOption={ props.handleDeleteOption }
         />
       )) }
